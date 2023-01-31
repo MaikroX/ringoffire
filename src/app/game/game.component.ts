22cc83d31b8a53ee8cb2ef173551ed93f72f2b24
Maik Langer <maiklanger@live.de>
2023-01-31 16:31:25 +0100

    add card animations

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game = new Game();
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      const card = this.game.stack.pop();
+      if (card) {
+        this.currentCard = card;
+        console.log(this.currentCard);
+        this.pickCardAnimation = true;
+
+        console.log('New Card: ' + this.currentCard);
+        console.log('Game ', this.game);
+
+        setTimeout(() => {
+          this.game.playedCards.push(this.currentCard);
+          this.pickCardAnimation = false;
+        }, 1000);
+      }
+    }
+  }
 }
